99a5a8c50635a324039c8b0e0714b437395b4509
saimaazfar110 <132021451+saimaazfar110@users.noreply.github.com>
2023-08-12 23:41:52 +0500

    2 files added

---
+++ portfolio-app/src/components/navbar.js
@@ -0,0 +1,14 @@
+import "./navbarstyles.css"
+
+import React from 'react'
+import {link} from "react-router-dom"
+
+const Navbar = () => {
+  return(
+    <div className="header">
+      <Link></Link>
+      </div>
+  )
+} 
+
+export default Navbar

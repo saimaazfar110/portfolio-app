a6ad0b02e2ae136fcccba43adcc9d1ff173d0613
saimaazfar110 <132021451+saimaazfar110@users.noreply.github.com>
2023-08-14 21:00:44 +0500

    1 file modified

--- portfolio-app/src/App.jsx
+++ portfolio-app/src/App.jsx
@@ -1,36 +1,23 @@
-import React  from "react";
-import "./index.css";
-import Home from "./routes/Home";
-import About from "./routes/About";
+ import React from "react";
+ import "./index.css";
+ import Home from "./routes/Home";
+ import About from "./routes/About"
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
 
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 
-
-function App() {
+function app() {
   return (
-
-  <>
- 
- <Routes>
-   <Route path="/Home" element={<Home/>} />
-   <Route path="/Project" element={<Project/>} />
-   <Route path="/About" element={<About/>} />
-   <Route path="/Contact" element={<Contact/>} />
-   </Routes>
-   </>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Project" element={<Project />} />
+      <Route path="/about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      </Routes>
+      </>
 
 
-      
-  
-
-  
-
-  
-  )}
-
-export default App;
-
-
-  
+  )
+}

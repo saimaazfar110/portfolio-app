29d8e98ebfe7cf539958c461c1e86fd862dd30d5
saimaazfar110 <132021451+saimaazfar110@users.noreply.github.com>
2023-08-14 13:45:03 +0500

    1 file modified

---
+++ portfolio-app/src/components/navbarstyles.js
@@ -0,0 +1,17 @@
+Header
+  display: flex;
+  justify=content: space-between;
+  align-items: center;
+  padding: 1rem 
+
+nav-menu {
+display: flex;
+}
+nav-menu li {
+  pandding: 0 1rem;
+}
+
+nav-menu li a {
+  font-size: 1.2rem;
+  font-weight: 500;
+};

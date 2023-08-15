c099fd8abeb6211a4a29842d99041e367214c846
saimaazfar110 <132021451+saimaazfar110@users.noreply.github.com>
2023-08-16 00:36:15 +0500

    1 file added, 1 deleted

--- portfolio-app/src/App..jsx
+++ portfolio-app/src/App..jsx
@@ -2,17 +2,17 @@
  import "./index.css";
  import Home from "./routes/Home";
  import About from "./routes/About"
-import Project from "./routes/Project";
-import Contact from "./routes/Contact";
+ import Project from "./routes/Project";
+ import Contact from "./routes/Contact";
 
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function APP() {
   return (
     <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Project" element={<Project />} />
+      <Route path="/project" element={<Project />} />
       <Route path="/about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       </Routes>
